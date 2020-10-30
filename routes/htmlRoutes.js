2749d1220cce3e952ced78208095d464f0342227
yerson <yerson.a06@gmail.com>
2020-10-29 22:31:35 -0400

    notes.html route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,14 @@
+const { notStrictEqual } = require("assert");
 const path = require("path");
 
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
      res.sendFile(path.join(__dirname, "/../public/index.html"));
+  });
+  
+  app.get("/notes", function(req, res) {
+     res.sendFile(path.join(__dirname, "/../public/notes.html"));
   });
   
 }
